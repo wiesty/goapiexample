302af3e3be53939208b16efb28fb5ceb24fcc49b
Wiesty <106901362+wiesty@users.noreply.github.com>
2024-09-10 17:19:45 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,10 +7,8 @@ import (
 )
 
 func main() {
-    // Router initialisieren
     router := api.SetupRouter()
 
-    // Server auf Port 8080 starten
     log.Println("Starting server on :8080")
     err := http.ListenAndServe(":8080", router)
     if err != nil {
